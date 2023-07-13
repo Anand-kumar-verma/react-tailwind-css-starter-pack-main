47d119c69141170bba8005fd4304ba73e0a2a677
Ajay Singh <aj50738@gmail.com>
2023-07-13 10:49:34 +0530

    Bharat NDR Page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 
 import Home from './Pages/Home'
+import NDR from "./Pages/NDR";
 
 function App() {
   return (
-         <Routes>
-            <Route path="/" element={<Home/>}></Route>
-         </Routes>
+    <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/ndr" element={<NDR />} />
+
+    </Routes>
   );
 }
 
